2380a6b0118dc9d74e6de99dbee559c66521a401
Eric Washington <eric.washington.me@gmail.com>
2018-01-24 08:36:13 -0500

    Initial Commit

---
+++ prop-listings/src/app/app.module.ts
@@ -0,0 +1,61 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
+import { AngularFireModule, AuthProviders, AuthMethods} from 'angularfire2';
+import {FirebaseService} from './services/firebase.service';
+import {FlashMessagesModule} from 'angular2-flash-messages';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListingsComponent } from './components/listings/listings.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListingComponent } from './components/listing/listing.component';
+import { AddListingComponent } from './components/add-listing/add-listing.component';
+import { EditListingComponent } from './components/edit-listing/edit-listing.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyA7FOQBI7y2rOFkdpilYCUJaxwilXI5SPg",
+    authDomain: "property-listing-app-1fa8e.firebaseapp.com",
+    databaseURL: "https://property-listing-app-1fa8e.firebaseio.com",
+    projectId: "property-listing-app-1fa8e",
+    storageBucket: "property-listing-app-1fa8e.appspot.com",
+    messagingSenderId: "499307569578"
+};
+
+const firebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Popup
+};
+
+const appRoutes: Routes = [
+  {path:'', component:HomeComponent},
+  {path: 'listings', component:ListingsComponent},
+  {path:'listing/:id', component:ListingComponent},
+  {path: 'add-listing', component:AddListingComponent},
+  {path:'edit-listing/:id', component:EditListingComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ListingsComponent,
+    NavbarComponent,
+    ListingComponent,
+    AddListingComponent,
+    EditListingComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    FlashMessagesModule,
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig),
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [FirebaseService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
